43f8288e390cf99fca6a9ddbb3a2bde5cd010071
Chris <tjuyinsh@gmail.com>
2016-06-06 19:30:02 +0800

    Data View Constructed

--- CityGuide/info/admin.py
+++ CityGuide/info/admin.py
@@ -1,14 +1,22 @@
 from django.contrib import admin
-from info.models import Categories, GcmTable, Pois, Preference, Region, SqliteSequence, UserT
+from info.models import Categories, Pois, Preference, Region, UserT
 
 # Register your models here.
 class PoisAdmin(admin.ModelAdmin):
 	list_display = ('name', 'intro')
 
+class CateGoriesAdmin(admin.ModelAdmin) :
+	list_display = ('name', 'image', 'marker')
+	
+class RegionAdmin(admin.ModelAdmin):
+	list_display = ('city', 'latitude', 'longitude', 'picture')
+	
+class UserTAdmin(admin.ModelAdmin):
+	list_display = ('ps', 'age', 'gender', 'p_vehicle')
+		
+		
+
 admin.site.register(Pois, PoisAdmin)
-admin.site.register(Categories)
-admin.site.register(GcmTable)
-admin.site.register(Preference)
-admin.site.register(Region)
-admin.site.register(SqliteSequence)
-admin.site.register(UserT)
+admin.site.register(Categories, CateGoriesAdmin)
+admin.site.register(Region, RegionAdmin)
+admin.site.register(UserT, UserTAdmin)
