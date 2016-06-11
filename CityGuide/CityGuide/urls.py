dfa2623d560253340577871d533e397621ddb9a9
Herman <officialzhanghang@gmail.com>
2016-06-11 22:12:06 +0800

    update index

--- CityGuide/CityGuide/urls.py
+++ CityGuide/CityGuide/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
+	url(r'^', include('info.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^push/', include('push.urls')),
 ]
